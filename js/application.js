a547237aeb3fc5ca8623e2fd2a70dcee84287f98
TehTooya <tehtooya@ubuntu.ubuntu-domain>
2012-10-21 19:48:51 -0500

    Now that I have a better grasp of Backbone collections, I refactored a lot of code, and added event handlers / images

--- js/application.js
+++ js/application.js
@@ -4,77 +4,135 @@ var App = {
 	},
 	events: {
 		init: function(){
-			this.populatePokedex();
+			this.backboneExtends();
+			this.backboneInstances();
+			
 			App.variables.session.currentUser = new App.variables.backbone.User();
 			this.handlers();
 		},
+		backboneInstances: function(){
+			var pokemonList = App.variables.session.pokemonList = new App.variables.backbone.PokemonList();
+			this.populatePokedex();	
+		},
+		backboneExtends: function(){
+
+			// Not in use yet
+			App.variables.backbone.User = Backbone.Model.extend({
+				defaults: {
+					name: "Trainer",
+					game: "Pokemon Black2"
+				}
+			});
+
+			// Pokemon Object (i.e Bulbasaur)			
+			App.variables.backbone.Pokemon = Backbone.Model.extend({
+				defaults: {
+					caught:false
+				},
+				toggleCaught: function() {
+					if(!this.get("caught")) {
+						this.set({"caught": true})
+
+					} else {
+						this.set({"caught": false})
+					}
+				}
+			});
+
+			// Collection of Pokemon Objects
+			App.variables.backbone.PokemonList = Backbone.Collection.extend({
+				model: App.variables.backbone.Pokemon
+			});
+
+			// View for individual Pokemon
+			App.variables.backbone.PokemonView = Backbone.View.extend({
+				initialize: function() {
+					this.model.on("change", this.render, this);
+					this.model.on("destroy", this.remove, this);
+				},
+				template: _.template('<div class="pokemon-inner" style="background-position: right <%= thumbnailPosition %>px;">\
+					<input type="checkbox" <% if(caught) print("checked=checked") %> /> <%= id %>: <%= name %></div>'),
+				tagName: "div",
+				className: "pokemon", 
+				render: function() {
+					var attributes = this.model.toJSON();					
+					this.$el.html(this.template(attributes));
+					return this;
+				},
+				remove: function() {
+					this.$el.remove();
+				},
+				events: {
+					"click .pokemon-inner": "toggleCaught",
+					"hover .pokemon-inner": "displayPokemonInfo"
+				},
+				toggleCaught: function() {
+					this.model.toggleCaught();
+				},
+				displayPokemonInfo: function() {
+					//Image Sources: http://pokemondb.net/			
+					$('.menu-3').html("<img src='"+this.model.get("image")+"' />"+this.model.get("name"));
+				}
+			});
+
+			// View for List of Pokemon
+			App.variables.backbone.PokemonListView = Backbone.View.extend({
+				initialize: function(){
+					this.collection.on('add', this.addOne, this);
+					this.collection.on('reset', this.addAll, this);
+				},
+				className: "pokemon-list",
+				render: function() {
+					this.addAll();
+				},
+				addAll: function() {
+					this.collection.forEach(this.addOne, this);
+				},
+				addOne: function(pokemon) {
+					var pokemonView = new App.variables.backbone.PokemonView({model:pokemon});
+					this.$el.append(pokemonView.render().el);
+				}
+			});
+
+		},
 		handlers: function(){
-			$('.pokeList').on("click", function(){
-				App.events.drawPokeDex($('.menu-2'));
+			$('.pokeList').live("click", function(){				
+				$('.menu-2').html(App.events.drawPokedex());
+				$('.menu-3').show();
 			});
-			$('.pokemonCheck').live("click", function(){
-				console.log($('.pokemonCheck:checked'))
-			});
+
 			$('.pokemon-list-item').live("click", function() {
 				$('.menu-3').show();
 				App.events.drawPokemon($('.menu-3'), $(this).attr("data-pokemon-id"));
 			});			
 		},
-		populatePokedex: function(){			
-			//https://raw.github.com/gist/1994205/84b771ac55d05d566f098030a48786c31cadb62d/pokedex.js
+		populatePokedex: function(){
+			var pokemonList = App.variables.session.pokemonList;
 			$.getJSON('js/pokedex.json', function(result){
-				for(var i = 0; i<result.length;i++) {
-					var tempPokemon = new App.variables.backbone.Pokemon(result[i])
-					App.variables.session.pokedex.push(tempPokemon);
-				}				
+				pokemonList.reset(result);
+				App.variables.session.pokemonListView = new App.variables.backbone.PokemonListView({collection: pokemonList});
 			});
 		},
-		drawPokeDex: function(element){
-			var pokemonList = new App.variables.backbone.PokedexView({model: App.variables.session.pokedex});			
+		drawPokedex: function(){
+			var pokemonList = new App.variables.backbone.PokemonListView({collection: App.variables.session.pokemonList});
 			pokemonList.render();
-			element.html(pokemonList.el);
+			return pokemonList.el;
 		},
-		drawPokemon: function(element, pokemonId) {
-			var pokemon = new App.variables.backbone.PokemonView({model: App.variables.session.pokedex[pokemonId]});
-			pokemon.render();
-			element.html(pokemon.el);
-		}
+
 	},
 	variables: {		
 		session: {
-			pokedex: new Array(),
+			pokemonList: {},
 			currentUser: {},
+			pokemonListView: {},
 		},
 		backbone: {
-			User: Backbone.Model.extend({
-				defaults: {
-					name: "Trainer",
-					game: "Pokemon Black2",
-					pokedex: {
-
-					}
-				}
-			}),		
-			Pokemon: Backbone.Model.extend({}),
-			PokedexView: Backbone.View.extend({
-				render: function() {
-					var html = "<ul class='pokemon-list'>";
-					for(var i=0;i<649;i++) {
-						html = html+ ("<li class='pokemon-list-item' style='background-position:right "+
-							(i*32*-1)+"px' data-pokemon-id='"+i+"'><input class='pokemonCheck' type='checkbox' name='pokemon' value='"+
-							this.model[i].get("dec")+"' data-pokemon='"+this.model[i].get("name")+"' />"+this.model[i].get("name")+"</li>");
-					}
-					html = html+ "</ul>";
-					$(this.el).html(html);
-				}
-			}),
-			PokemonView: Backbone.View.extend({
-				render: function() {
-					var html = $('<div>');
-					html.append(this.model.get("dec")+": "+this.model.get("name"));
-					$(this.el).html(html.html());
-				}
-			})
+			User: {},		
+			Pokemon: {},
+			PokemonList: {},
+			PokemonListView: {},
+			PokemonView: {},
+			PokemonListView: {}
 		}		
 	}
 }
